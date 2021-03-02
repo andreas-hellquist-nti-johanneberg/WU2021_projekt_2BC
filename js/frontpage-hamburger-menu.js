0f4adbb96194de48e1f459907fae8f04e76c5e04
Andreas Hellquist <spare7297@gmail.com>
2021-03-02 10:43:34 +0100

    Fix frontpage hamburger menu on short phones
    Previously the font size of the hamburger menu was to large, so it would
    get cut of before exposing all entries. This makes the font smaller to
    fit on shorter phones.
    
    The issue of the user being able to scroll the webpage behind the menu
    has also been taken care of.

--- js/frontpage-hamburger-menu.js
+++ js/frontpage-hamburger-menu.js
@@ -1,12 +1,13 @@
 const toggleHamburgerMenu = () => {
     document.querySelector ("nav#mobile-hamburger-menu").classList.toggle ("show");
     document.querySelector ("div.fade-layer").classList.toggle ("show");
+
+    // Stop the scrolling of the webpage below
+    document.querySelector ("body").classList.toggle ("stop-scroll");
 }
 
 document.querySelector ("nav#top-nav-mobile div#hamburger-menu-button").addEventListener ("click", toggleHamburgerMenu);
-
 // If the user clicks on one of the links the menu should obviously hide
 document.querySelector ("nav#mobile-hamburger-menu").addEventListener ("click", toggleHamburgerMenu);
-
 // If the user clicks outside of the menu it should hide
 document.querySelector ("div.fade-layer").addEventListener ("click", toggleHamburgerMenu);
